db01f622bb714ab1f63c28a536a3c9b7ff65ffb8
KhushiiRathore <khushirathore1630@gmail.com>
2026-02-12 09:20:54 +0530

    Fix port config for Railway serverless

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use("/api/v1/dashboard" ,dashboardRoutes);
 //Server uploads folder
 app.use("/uploads" , express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
